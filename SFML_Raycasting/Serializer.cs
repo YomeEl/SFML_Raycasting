9b8ca199cdd47fd8612256e944b34aa67eff1c37
Ярослав <umpepatopChannel@gmail.com>
2019-05-19 21:38:32 +0500

    Hello

--- SFML_Raycasting/Serializer.cs
+++ SFML_Raycasting/Serializer.cs
@@ -8,21 +8,17 @@ namespace Raycasting
         public static void Serialize<T>(T target, string filename)
         {
             BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream f = new FileStream(filename, FileMode.Create))
-            {
-                formatter.Serialize(f, target);
-                f.Close();
-            }
+            FileStream f = new FileStream(filename, FileMode.Create);
+            formatter.Serialize(f, target);
+            f.Close();
         }
 
         public static void Deserialize<T>(out T target, string filename)
         {
             BinaryFormatter formatter = new BinaryFormatter();
-            using (FileStream f = new FileStream(filename, FileMode.Open))
-            {
-                target = (T)formatter.Deserialize(f);
-                f.Close();
-            }
+            FileStream f = new FileStream(filename, FileMode.Open);
+            target = (T)formatter.Deserialize(f);
+            f.Close();
         }
     }
 }
